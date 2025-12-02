0f53bfa0af3c26a2bc2e21bb9c25dd0a306d0475
anglczbla <angelicaizabella90@gmail.com>
2025-12-02 22:44:37 +0700

    regist with tan query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./auth/Login";
+import Register from "./auth/Register";
 import TodoForm from "./components/TodoForm";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div>
-      <TodoForm />
-    </div>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/todo" element={<TodoForm />} />
+        </Routes>
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/auth/Register.jsx
@@ -0,0 +1,98 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const backendUrl = import.meta.env.VITE_BE_URL;
+  const [formRegist, setFormRegist] = useState({
+    email: "",
+    password: "",
+    role: "",
+    username: "",
+  });
+  const [errors, setErrors] = useState(null);
+
+  const handleRegist = (e) => {
+    const { name, value } = e.target;
+    setFormRegist({ ...formRegist, [name]: value });
+  };
+
+  const registrasi = useMutation({
+    mutationFn: (regist) => {
+      return axios.post(backendUrl + "register", regist);
+    },
+    onSuccess: () => {
+      alert("registration success");
+      navigate("/");
+      queryClient.invalidateQueries({ queryKey: ["regist"] });
+      setFormRegist({
+        email: "",
+        password: "",
+        role: "",
+        username: "",
+      });
+    },
+    onError: (error) => {
+      console.error(error);
+      const newError = error.response.data.errors;
+      setErrors(newError);
+      alert("failed regist");
+    },
+  });
+
+  const registUser = (e) => {
+    e.preventDefault();
+    setErrors(null);
+    registrasi.mutate(formRegist);
+  };
+  return (
+    <div>
+      {errors !== null && errors.length > 0 ? (
+        <div className="mb-4 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
+          <p className="font-semibold">Gagal registrasi:</p>
+          <ul className="list-disc list-inside mt-2">
+            {errors.map((err, index) => (
+              <li key={index}>{Object.values(err).join(", ")}</li>
+            ))}
+          </ul>
+        </div>
+      ) : null}
+      <form onSubmit={registUser}>
+        <input
+          type="email"
+          name="email"
+          value={formRegist.email}
+          placeholder="Input Email"
+          onChange={handleRegist}
+        />
+        <input
+          type="password"
+          name="password"
+          value={formRegist.password}
+          placeholder="Input Password"
+          onChange={handleRegist}
+        />
+        <input
+          type="text"
+          name="role"
+          value={formRegist.role}
+          placeholder="Input Role"
+          onChange={handleRegist}
+        />
+        <input
+          type="text"
+          name="username"
+          value={formRegist.username}
+          placeholder="Input Username"
+          onChange={handleRegist}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
