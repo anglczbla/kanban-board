5327dbf91d6794ad0bd0297087f4e13779c53732
anglczbla <angelicaizabella90@gmail.com>
2025-12-02 23:05:29 +0700

    login with tanquery

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,5 +1,81 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 const Login = () => {
-  return <div>Login</div>;
+  const queryClient = useQueryClient();
+  const navigate = useNavigate();
+  const backendUrl = import.meta.env.VITE_BE_URL;
+  const [formLogin, setFormLogin] = useState({
+    username: "",
+    password: "",
+  });
+  const [errors, SetErrors] = useState(null);
+  console.log("isi errors", errors);
+
+  const handleLogin = (e) => {
+    const { name, value } = e.target;
+    setFormLogin({ ...formLogin, [name]: value });
+  };
+
+  const loginUser = useMutation({
+    mutationFn: (formLogin) => {
+      return axios.post(backendUrl + "login", formLogin);
+    },
+    onSuccess: () => {
+      alert("login success");
+      navigate("/todo");
+      queryClient.invalidateQueries({ queryKey: ["login"] });
+
+      setFormLogin({
+        username: "",
+        password: "",
+      });
+    },
+    onError: (error) => {
+      console.error(error);
+      const newError = error.response.data.message;
+      console.log("isi new error", newError);
+
+      SetErrors(newError);
+      alert("failed login");
+    },
+  });
+
+  const submitLogin = (e) => {
+    e.preventDefault();
+    SetErrors(null);
+    loginUser.mutate(formLogin);
+  };
+
+  return (
+    <div>
+      {errors !== null && errors.length > 0 ? (
+        <div className="mb-4 p-4 bg-red-100 border border-red-400 text-red-700 rounded">
+          <p className="font-semibold">Gagal login:</p>
+          {errors}
+        </div>
+      ) : null}
+      <form onSubmit={submitLogin}>
+        <input
+          type="text"
+          name="username"
+          value={formLogin.username}
+          placeholder="Input username"
+          onChange={handleLogin}
+        />
+        <input
+          type="password"
+          name="password"
+          value={formLogin.password}
+          placeholder="Input Password"
+          onChange={handleLogin}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
